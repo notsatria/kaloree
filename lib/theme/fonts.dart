78d0fcf7aa68ea2e07be03acff0274ebf87027a4
notsatria <buanasatriaa@gmail.com>
2024-03-25 22:02:23 +0700

    feat: add fonts and sizes theme

---
+++ lib/theme/fonts.dart
@@ -0,0 +1,22 @@
+import 'dart:ui';
+
+TextStyle interLight = TextStyle(
+  fontFamily: 'Inter',
+  fontWeight: FontWeight.w300,
+);
+TextStyle interRegular = TextStyle(
+  fontFamily: 'Inter',
+  fontWeight: FontWeight.w400,
+);
+TextStyle interMedium = TextStyle(
+  fontFamily: 'Inter',
+  fontWeight: FontWeight.w500,
+);
+TextStyle interSemiBold = TextStyle(
+  fontFamily: 'Inter',
+  fontWeight: FontWeight.w600,
+);
+TextStyle interBold = TextStyle(
+  fontFamily: 'Inter',
+  fontWeight: FontWeight.w700,
+);
