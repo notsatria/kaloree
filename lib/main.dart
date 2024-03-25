6e0751c4fec1527a2b5eef317dd3ae9d311c1aac
notsatria <buanasatriaa@gmail.com>
2024-03-25 21:58:59 +0700

    feat: implement dynamic color and add static fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
+import 'package:kaloree/theme/color_schemes.g.dart';
+import 'package:kaloree/theme/custom_color.g.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MainApp());
 }
 
@@ -9,12 +13,39 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return DynamicColorBuilder(
+        builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+      ColorScheme lightScheme;
+      ColorScheme darkScheme;
+
+      if (lightDynamic != null && darkDynamic != null) {
+        lightScheme = lightDynamic.harmonized();
+        lightCustomColors = lightCustomColors.harmonized(lightScheme);
+
+        // Repeat for the dark color scheme.
+        darkScheme = darkDynamic.harmonized();
+        darkCustomColors = darkCustomColors.harmonized(darkScheme);
+      } else {
+        // Otherwise, use fallback schemes.
+        lightScheme = lightColorScheme;
+        darkScheme = darkColorScheme;
+      }
+
+      return MaterialApp(
+        theme: ThemeData(
+          useMaterial3: true,
+          scaffoldBackgroundColor: Colors.white,
+          fontFamily: 'Inter',
+          colorScheme: lightScheme,
+          extensions: [lightCustomColors],
         ),
-      ),
-    );
+        darkTheme: ThemeData(
+          useMaterial3: true,
+          fontFamily: 'Inter',
+          colorScheme: darkScheme,
+          extensions: [darkCustomColors],
+        ),
+      );
+    });
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import connectivity_plus
+import dynamic_color
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
