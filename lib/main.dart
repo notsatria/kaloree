176cdb486b178306a4d18ea9a7ea59b13efc2360
notsatria <buanasatriaa@gmail.com>
2024-06-18 12:22:27 +0700

    improve: check if user is already signed in or not

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dynamic_color/dynamic_color.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -6,10 +8,14 @@ import 'package:kaloree/core/routes/app_route.dart';
 import 'package:kaloree/core/theme/app_theme.dart';
 import 'package:kaloree/core/theme/color_schemes.g.dart';
 import 'package:kaloree/core/theme/custom_color.g.dart';
+import 'package:kaloree/core/widgets/loading.dart';
 import 'package:kaloree/features/assesment/presentation/bloc/assesment_bloc.dart';
+import 'package:kaloree/features/assesment/presentation/views/personal_information_view.dart';
+import 'package:kaloree/features/assesment/presentation/widgets/custom_error_view.dart';
 import 'package:kaloree/features/auth/presentaion/bloc/auth_bloc.dart';
 import 'package:kaloree/features/catatan/presentation/bloc/catatan_bloc.dart';
 import 'package:kaloree/features/main_menu/presentation/cubit/bottom_navigation_cubit.dart';
+import 'package:kaloree/features/main_menu/presentation/views/main_view.dart';
 import 'package:kaloree/features/onboarding/presentation/cubit/onboarding_cubit.dart';
 import 'package:kaloree/features/onboarding/presentation/views/onboarding_view.dart';
 import 'package:kaloree/features/scan/presentation/bloc/image_classification_bloc.dart';
@@ -65,11 +71,45 @@ class MainApp extends StatelessWidget {
         child: MaterialApp(
           theme: AppTheme.lightTheme(lightScheme),
           darkTheme: AppTheme.darkTheme(darkScheme),
-          home: const OnBoardingView(),
+          home: FutureBuilder<Widget>(
+            future: _checkUserSignedIn(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Scaffold(body: Loading());
+              } else if (snapshot.hasError) {
+                return Scaffold(
+                  body: ErrorView(message: snapshot.error.toString()),
+                );
+              } else {
+                return snapshot.data ?? const OnBoardingView();
+              }
+            },
+          ),
           initialRoute: AppRoute.onboarding,
           onGenerateRoute: AppRoute.onGenerateRoute,
         ),
       );
     });
+  }
+
+  Future<Widget> _checkUserSignedIn() async {
+    final auth = FirebaseAuth.instance;
+    final firestore = FirebaseFirestore.instance;
+
+    if (auth.currentUser != null) {
+      bool isAssesmentComplete = await firestore
+          .collection('users')
+          .doc(auth.currentUser!.uid)
+          .get()
+          .then((doc) => doc['isAssesmentComplete'] as bool? ?? false);
+
+      if (isAssesmentComplete) {
+        return const MainView();
+      } else {
+        return const PersonalInformationView();
+      }
+    } else {
+      return const OnBoardingView();
+    }
   }
 }
