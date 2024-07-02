b0acd857a478b0590403c3ba8ac29c59081e5faa
notsatria <buanasatriaa@gmail.com>
2024-07-02 14:54:20 +0700

    bugfix: fixing bug on bottom navbar onboarding_view

--- lib/features/onboarding/presentation/views/onboarding_view.dart
+++ lib/features/onboarding/presentation/views/onboarding_view.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:kaloree/features/onboarding/presentation/cubit/onboarding_cubit.dart';
-import 'package:kaloree/features/onboarding/presentation/views/widgets/onboarding_widget.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:gap/gap.dart';
+import 'package:kaloree/core/routes/app_route.dart';
 import 'package:kaloree/core/theme/color_schemes.g.dart';
 import 'package:kaloree/core/theme/colors.dart';
 import 'package:kaloree/core/theme/sizes.dart';
-import 'package:kaloree/core/routes/app_route.dart';
 import 'package:kaloree/core/widgets/custom_button.dart';
+import 'package:kaloree/features/onboarding/presentation/cubit/onboarding_cubit.dart';
+import 'package:kaloree/features/onboarding/presentation/views/widgets/onboarding_widget.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnBoardingView extends StatefulWidget {
@@ -39,13 +41,30 @@ class _OnBoardingViewState extends State<OnBoardingView> {
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.symmetric(
             horizontal: margin_20, vertical: margin_20),
-        child: CustomFilledButton(
-          text: 'Daftar Sekarang',
-          backgroundColor: Colors.white,
-          textColor: lightColorScheme.primary,
-          onTap: () {
-            goReplacementNamed(context, AppRoute.login);
-          },
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            CustomFilledButton(
+              text: 'Daftar Sekarang',
+              backgroundColor: Colors.white,
+              textColor: lightColorScheme.primary,
+              onTap: () {
+                goReplacementNamed(context, AppRoute.login);
+              },
+            ),
+            const Gap(15),
+            CustomOutlinedButton(
+              text: 'Masuk dengan Google',
+              onTap: () {},
+              outlineColor: lightColorScheme.outline,
+              textColor: lightColorScheme.outline,
+              outlineWidth: 2,
+              leadingIcon: FaIcon(
+                FontAwesomeIcons.google,
+                color: lightColorScheme.outline,
+              ),
+            ),
+          ],
         ),
       ),
       body: BlocBuilder<OnBoardingCubit, int>(
