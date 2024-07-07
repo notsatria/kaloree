8eb9fb0e412fa8be21ee61eda97787e404b8fbae
notsatria <buanasatriaa@gmail.com>
2024-07-07 11:49:03 +0700

    feat: implement get recommendation repositories

--- lib/features/profile/data/repositories/profile_repository_impl.dart
+++ lib/features/profile/data/repositories/profile_repository_impl.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:fpdart/fpdart.dart';
 import 'package:kaloree/core/errors/failure.dart';
 import 'package:kaloree/core/model/user_model.dart';
+import 'package:kaloree/features/home/data/model/recommendation.dart';
 import 'package:kaloree/features/profile/data/datasource/profile_remote_datasource.dart';
 import 'package:kaloree/features/profile/domain/repositories/profile_repository.dart';
 
@@ -26,6 +27,18 @@ class ProfileRepositoryImpl implements ProfileRepository {
     try {
       final result =
           await remoteDataSource.editProfile(fullName: fullName, image: image);
+      return right(result);
+    } catch (e) {
+      return left(Failure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<Recommendation>>> getRecommendation(
+      {required bool isSportRecommendation}) async {
+    try {
+      final result = await remoteDataSource.getRecommendation(
+          isSportRecommendation: isSportRecommendation);
       return right(result);
     } catch (e) {
       return left(Failure(e.toString()));
--- lib/features/profile/domain/repositories/profile_repository.dart
+++ lib/features/profile/domain/repositories/profile_repository.dart
@@ -3,9 +3,12 @@ import 'dart:io';
 import 'package:fpdart/fpdart.dart';
 import 'package:kaloree/core/errors/failure.dart';
 import 'package:kaloree/core/model/user_model.dart';
+import 'package:kaloree/features/home/data/model/recommendation.dart';
 
 abstract interface class ProfileRepository {
   Future<Either<Failure, UserModel>> getUserData();
   Future<Either<Failure, void>> editProfile(
-      {required String fullName,  File? image});
+      {required String fullName, File? image});
+  Future<Either<Failure, List<Recommendation>>> getRecommendation(
+      {required bool isSportRecommendation});
 }
